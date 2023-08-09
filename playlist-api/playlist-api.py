5c79e96f0e959349eb7295d3260f350d167f9006
somT-oss <somtochukwuuchegbu@gmail.com>
2023-08-09 10:21:35 +0100

    add: comments to code base

--- playlist-api/playlist-api.py
+++ playlist-api/playlist-api.py
@@ -3,20 +3,26 @@ from redis import Redis
 import os
 from flask_cors import CORS
 
+# Flask app
 app = Flask(__name__)
+
+# CORS configuration
 CORS(app, resources={r"*": {"origins": "*"}})
 
+# Redis DB connection function
 def connect(host, port): 
     redis_db = Redis(host=host, port=port, decode_responses=True)
 
     return redis_db
 
-
+# GET route for all playlists
 @app.route('/get-playlists', methods=['GET'])
 def get_all_playlists():
 
+    # Redis DB connection object
     redis_db = connect(os.getenv('REDIS_HOST'), os.getenv('REDIS_PORT'))
 
+    # Error handling for connection failure to Redis
     if redis_db is None: 
         return {
             "message": "Could not connect to DB!",
@@ -27,6 +33,7 @@ def get_all_playlists():
     if redis_db is None:
         return "Error occurred trying to connect to DB"
     
+    # All playlist hashes in the redis key value store
     hash_keys = ['playlist:1', 'playlist:2', 'playlist:3']
     playlists = []
 
@@ -34,6 +41,7 @@ def get_all_playlists():
         data = redis_db.hgetall(key)
         playlists.append(data)
     
+    # Return all playlists in the DB
     return {
         "playlists": playlists,
         "statusCode": 200
--- videos-api/videos-api.py
+++ videos-api/videos-api.py
@@ -3,28 +3,36 @@ from redis import Redis
 import os
 from flask_cors import CORS
 
+# Flask app
 app = Flask(__name__)
+
+# CORS configuration
 CORS(app, resources={r"*": {"origins": "*"}})
 
-
+# Redis DB connection function
 def connect(host, port): 
     redis_db = Redis(host=host, port=port, decode_responses=True)
 
     return redis_db
 
-
+# GET route for video
 @app.route('/get-video/<video>', methods=['GET'])
 def get_video(video):
 
+    # Redis DB connection object
     redis_db = connect(os.getenv('REDIS_HOST'), os.getenv('REDIS_PORT'))
 
+    # Error handling for connection failure to Redis
     if redis_db is None: 
         return {
             "message": "Could not connect to DB!",
             "statusCode": 400
         }
     
+    # Get video in the DB by using the video_id
     video = redis_db.hgetall(video)
+    
+    # Return video from the DB
     return {
         "video": video, 
         "statusCode": 200
