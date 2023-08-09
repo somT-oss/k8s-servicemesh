e228a4db838a271a1cfe54f16f8a24b7ac90b954
somT-oss <somtochukwuuchegbu@gmail.com>
2023-08-09 10:17:20 +0100

    refactor: switched to redis DB

--- Frontend/env.js
+++ Frontend/env.js
@@ -2,6 +2,6 @@
 window.process = {
   env: {
     REACT_APP_PLAYLIST_API_URL: 'http://127.0.0.1:5000/get-playlists',
-    REACT_APP_VIDEO_API_URL: 'http://127.0.0.1:5001'
+    REACT_APP_VIDEO_API_URL: 'http://127.0.0.1:5001/<video_id>'
   }
 };
--- videos-api/videos-api.py
+++ videos-api/videos-api.py
@@ -1,90 +1,34 @@
 from flask import Flask, request
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-from datetime import datetime
-import uuid
-import ast
+from redis import Redis
 import os
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
 
-URI = f"mongodb+srv://{os.getenv('MONGO_USERNAME')}:{os.getenv('MONGO_PASSWORD')}@k8s-cluster-db.qjpa5yf.mongodb.net/?retryWrites=true&w=majority"
 
+def connect(host, port): 
+    redis_db = Redis(host=host, port=port, decode_responses=True)
 
-def connect(URI: str): 
-    # Set the Stable API version when creating a new client
-    client = MongoClient(URI, server_api=ServerApi('1'))
-                            
-    # Send a ping to confirm a successful connection
-    try:
-        client.admin.command('ping')
-        return client
-    except Exception as e:
-        print(e)
-        return None
+    return redis_db
 
-
-@app.route("/create-video", methods=['POST'])
-def create_videos():
-    video_uuid = str(uuid.uuid4())
-    video = videos_in_tech[2]
-    title = request.json['title']
-    description = request.json['description']
-    date_created = datetime.now()
-
-    video_dict = {
-        "_id": video_uuid,
-        "title": title,
-        "video": video,
-        "description": description,
-        "date_created": date_created
-    }
-
-    db_connection = connect(URI)
-    if db_connection is None:
-        return {
-            "message": "Could not connect to DB!",
-            "statusCode": 400
-        }
-        
-    db = db_connection['videos-db']
-    videos_db = db['videos-collection']
-    try:
-        videos_db.insert_one(video_dict)
-        return {
-            "message": "Video successfully created",
-            "statusCode": 200
-        }
-    except Exception as e:
-        return {
-            "message": e,
-            "statusCode": 400
-        }
 
 @app.route('/get-video/<video>', methods=['GET'])
 def get_video(video):
 
-    db_connection = connect(URI)
-    if db_connection is None:
+    redis_db = connect(os.getenv('REDIS_HOST'), os.getenv('REDIS_PORT'))
+
+    if redis_db is None: 
         return {
             "message": "Could not connect to DB!",
             "statusCode": 400
         }
     
-    db = db_connection['videos-db']
-    videos_db = db['videos-collection']
-
-    try:
-        video = videos_db.find_one({"video": video})
-        return video
-    except Exception as e:
-        print("Error: ", e, " occurred")
-        return {
-            "message": "An error occurred",
-            "statusCode": 400
-        }
+    video = redis_db.hgetall(video)
+    return {
+        "video": video, 
+        "statusCode": 200
+    }
 
 
 if __name__ == '__main__':
